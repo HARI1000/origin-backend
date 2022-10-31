c8cefdabfc4734eea1e51ad40fe38aa03466955c
Harishankar <you@example.com>
2022-10-31 21:41:23 +0530

    3d  commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const FeedbackModel=require('./models/feedback')
 const prgrmdatamodel=require('./models/senddata')
 app.use(express.json());
 app.use(cors({
-    origin:["http://localhost:3000","https://origintranscoder-app.onrender.com"]
+    origin:["http://localhost:3000","https://origintranscoder.onrender.com"]
 }
 ));
 
